6ffe46dabef34aa7efeefcc99262bed04c35b28e
dbalampanidis <dbalampanidis@gmail.com>
2023-11-30 22:30:08 +0000

    add footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ import {
 } from "react-icons/md";
 
 import Country from "./components/Country";
+import Footer from "./components/Footer";
 import CountryDetails from "./pages/CountryDetails";
 
 import "./App.css";
@@ -145,6 +146,7 @@ function App() {
                     )}
                   </section>
                 </div>
+                <Footer />
               </>
             )
           }
---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import { ReactComponent as GitHubLogo } from "../images/logo-github.svg";
+
+const Footer = () => {
+  return (
+    <footer>
+      <a href="https://github.com/dbalampanidis/rest-countries-api">
+        GitHub
+        <GitHubLogo />
+      </a>
+    </footer>
+  );
+};
+
+export default Footer;

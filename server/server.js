a3fed1d02174f290b0342de5352a304ed6a14396
dbalampanidis <dbalampanidis@gmail.com>
2023-11-30 15:38:46 +0000

    add /countryByName/:country endpoint

--- server/server.js
+++ server/server.js
@@ -9,6 +9,19 @@ app.get("/allCountries", async (req, res) => {
   res.json(countries);
 });
 
+app.get("/countryByName/:country", async (req, res) => {
+  const { country } = req.params;
+  try {
+    const response = await axios.get(
+      `https://restcountries.com/v3.1/name/${country}`
+    );
+    const countries = response.data;
+    res.json(countries);
+  } catch (error) {
+    res.json(error);
+  }
+});
+
 app.listen(5000, () => {
   console.log("server listening to port 5000");
 });

6478f58d135d7db38c33d860de0c8fc7ef85131f
dbalampanidis <dbalampanidis@gmail.com>
2023-11-30 15:36:25 +0000

    add /allCountries endpoint

--- server/server.js
+++ server/server.js
@@ -1,6 +1,13 @@
 const express = require("express");
+const axios = require("axios");
 
 const app = express();
+
+app.get("/allCountries", async (req, res) => {
+  const response = await axios.get("https://restcountries.com/v3.1/all");
+  const countries = response.data;
+  res.json(countries);
+});
 
 app.listen(5000, () => {
   console.log("server listening to port 5000");
